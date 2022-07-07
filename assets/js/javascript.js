3f8950d9d405acbcacfc212fa625d881d88021cb
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-07-07 15:13:52 -0400

    named constants

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,11 @@
 const intro_button = document.querySelector(".intro_button button");
+const rule_box = document.querySelector(".rule_box");
+//const start_button = document.querySelector(".");
+const quiz_box = document.querySelector(".quiz.box");
+const results = document.querySelector(".results");
+const multiple_choice = document.querySelector(".multiple_choice");
+const timeText = document.querySelector(".timer .time_left_text");
+const timeCount = document.querySelector(".timer .timer_sec");
 
 //intro button
 intro_button.onclick = ()=>{
